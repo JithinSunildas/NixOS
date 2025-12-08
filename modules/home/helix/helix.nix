d4958f3829854c1559480d8dbffd3de6038a1490
JithinSunildas <jithinsunildas6@gmail.com>
2025-12-08 16:58:01 +0530

    home: 2025-12-08_16:58

--- modules/home/helix/helix.nix
+++ modules/home/helix/helix.nix
@@ -6,13 +6,8 @@ let
   editableConfigFile = ./live.toml;
 in {
   programs.helix.enable = true;
+  stylix.targets.helix.enable = true;
   
-  home.file = {
-    ".config/helix/config.toml" = {
-      source = editableConfigFile;
-    };
-  };
-
   home.sessionVariables = {
     HELIX_CONFIG = "${config.home.homeDirectory}/.config/helix"; 
   };
