d6f4e50414d5de6374e19fddd1aa4d24583cd455
JithinSunildas <jithinsunildas6@gmail.com>
2025-12-17 18:49:36 +0530

    home: 2025-12-17_18:49

--- modules/home/neovim/config/init.lua
+++ modules/home/neovim/config/init.lua
@@ -41,5 +41,5 @@ require("lazy").setup("plugins.init", {
 require("lsp")
 require("plugins.setup")
 require("plugins.extra")
--- vim.cmd("colorscheme base16_default")
+vim.cmd("colorscheme base16")
 vim.api.nvim_set_hl(0, "CursorLineNr", { bold = true })
