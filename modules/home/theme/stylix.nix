dee1bd82f02cc6cc10d79282f236dcf6691c1e2e
JithinSunildas <jithinsunildas6@gmail.com>
2025-12-20 11:21:35 +0530

    home: 2025-12-20_11:21

--- modules/home/theme/stylix.nix
+++ modules/home/theme/stylix.nix
@@ -7,6 +7,9 @@
     stylix = {
         enable = true;
         base16Scheme = "${pkgs.base16-schemes}/share/themes/kanagawa-dragon.yaml";
+        override = {
+          base00 = "1C1E25"; 
+        };
         image = ./wallpapers/demo.jpg;
         polarity = "dark";
 
