3745281136bf2969f12f3839991c1a17d425830f
JithinSunildas <jithinsunildas6@gmail.com>
2025-12-23 13:53:42 +0530

    home: 2025-12-23_13:53

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -2,19 +2,37 @@
 {
   programs.tmux = {
     enable = true;
-    prefix = "C-Space";
+    prefix = "A-Space";
     keyMode = "vi";
-    plugins = [
+    mouse = true;
+    baseIndex = 1;
+
+    plugins = with pkgs.tmuxPlugins; [
+      yank    
     ];
-    extraConfig = ''
-      set -g @plugin 'tmux-plugins/tpm'
-      set -g @plugin 'tarquibrian/vanzi'
+    sensible
+      extraConfig = ''
+      bind -n 'M-f' display-popup -E -w 80% -h 80% "fish"
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+      unbind '"'
+      unbind %
+
+# Smart pane switching with awareness of Vim splits.
+# This works with 'christoomey/vim-tmux-navigator' in Neovim
+      bind-key -n 'A-h' select-pane -L
+      bind-key -n 'A-j' select-pane -D
+      bind-key -n 'A-k' select-pane -U
+      bind-key -n 'A-l' select-pane -R
+
+# --- Theme & Visuals ---
+      set -g status-position top 
       set -g @theme_variant "vanzi"
       set -g @bg_transparency "on"
-          
+
+# If use external plugins manually:
       run-shell "~/.config/tmux/plugins/vanzi-tmux-theme/vanzi.tmux"
-      run '~/.config/tmux/plugins/tpm/tpm'
-    '';
-    mouse = true;
+# run '~/.config/tmux/plugins/tpm/tpm' # Usually not needed if using nix plugins
+      '';
   };
 }
