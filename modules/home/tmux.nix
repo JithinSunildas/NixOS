23a5c1f5d88a92ff4b2b0a5363605b2af824b163
JithinSunildas <jithinsunildas6@gmail.com>
2025-12-23 14:10:07 +0530

    home: 2025-12-23_14:10

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -1,38 +1,58 @@
+# ~/nix-config/modules/home/tmux.nix
+
 { config, pkgs, ... }:
+
 {
   programs.tmux = {
     enable = true;
-    prefix = "A-Space";
-    keyMode = "vi";
+    prefix = "C-Space";
     mouse = true;
+    terminal = "screen-256color";
     baseIndex = 1;
-
-    plugins = with pkgs.tmuxPlugins; [
-      yank    
-        sensible
-    ];
+    escapeTime = 0;
+    historyLimit = 10000;
+    
     extraConfig = ''
-      bind -n 'M-f' display-popup -E -w 80% -h 80% "fish"
-      bind | split-window -h -c "#{pane_current_path}"
-      bind - split-window -v -c "#{pane_current_path}"
+      # Vim-like pane navigation
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+      
+      # Vim-like pane resizing
+      bind -r H resize-pane -L 5
+      bind -r J resize-pane -D 5
+      bind -r K resize-pane -U 5
+      bind -r L resize-pane -R 5
+      
+      # Split panes using | and -
+      bind | split-window -h
+      bind - split-window -v
       unbind '"'
       unbind %
-
-# Smart pane switching with awareness of Vim splits.
-# This works with 'christoomey/vim-tmux-navigator' in Neovim
-      bind-key -n 'A-h' select-pane -L
-      bind-key -n 'A-j' select-pane -D
-      bind-key -n 'A-k' select-pane -U
-      bind-key -n 'A-l' select-pane -R
-
-# --- Theme & Visuals ---
-      set -g status-position top 
-      set -g @theme_variant "vanzi"
-      set -g @bg_transparency "on"
-
-# If use external plugins manually:
-      run-shell "~/.config/tmux/plugins/vanzi-tmux-theme/vanzi.tmux"
-# run '~/.config/tmux/plugins/tpm/tpm' # Usually not needed if using nix plugins
-      '';
+      
+      # Reload config
+      bind r source-file ~/.config/tmux/tmux.conf \; display "Reloaded!"
+      
+      # Minimal status bar
+      set -g status-position bottom
+      set -g status-style 'bg=default fg=colour8'
+      set -g status-left ""
+      set -g status-right ""
+      set -g status-justify left
+      
+      set -g window-status-current-style 'fg=colour7'
+      set -g window-status-style 'fg=colour8'
+      set -g window-status-format ' #I:#W '
+      set -g window-status-current-format ' #I:#W '
+      
+      # Pane borders
+      set -g pane-border-style 'fg=colour8'
+      set -g pane-active-border-style 'fg=colour8'
+      
+      # Remove clutter
+      set -g status-left-length 0
+      set -g status-right-length 0
+    '';
   };
 }
