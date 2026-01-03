855550ebb69d692d654775d93ee3d4aacf9199c6
JithinSunildas <jithinsunildas6@gmail.com>
2026-01-03 11:51:15 +0530

    home: 2026-01-03_11:51

--- modules/home/neovim/config/lua/plugins/init.lua
+++ modules/home/neovim/config/lua/plugins/init.lua
@@ -21,7 +21,6 @@ return {
   "nvim-telescope/telescope.nvim",
   { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   "nvim-lua/plenary.nvim",
-  "nvim-telescope/project.nvim",
 
   -- Find and Replace
   'nvim-pack/nvim-spectre',
@@ -54,6 +53,7 @@ return {
 
   -- Extra plugins
   "nvim-flutter/flutter-tools.nvim",
+  "ahmedkhalf/project.nvim",
   "folke/flash.nvim",
   "j-hui/fidget.nvim",
   "echasnovski/mini.surround",
