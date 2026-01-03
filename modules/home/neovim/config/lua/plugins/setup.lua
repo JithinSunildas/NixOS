8c11f15bdd23a4a5d8714f930203e093789ae2dc
JithinSunildas <jithinsunildas6@gmail.com>
2026-01-03 11:53:14 +0530

    home: 2026-01-03_11:53

--- modules/home/neovim/config/lua/plugins/setup.lua
+++ modules/home/neovim/config/lua/plugins/setup.lua
@@ -191,7 +191,7 @@ dashboard.section.header.val = {
 dashboard.section.buttons.val = {
   dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
   dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
-  dashboard.button("p", "󰉋  Projects", ":Telescope projects <CR>"),
+  dashboard.button("p", "  Projects", ":Telescope projects <CR>"),
   dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
   dashboard.button("g", "  Find text", ":Telescope live_grep <CR>"),
   dashboard.button("q", "  Quit", ":qa<CR>"),
