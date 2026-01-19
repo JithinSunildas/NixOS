07ba0e80dd00154e1e18e8f2d8fdd4c68e6c7092
JithinSunildas <jithinsunildas6@gmail.com>
2026-01-19 19:42:36 +0530

    home: 2026-01-19_19:42

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -29,14 +29,14 @@
         '';
       }
       {
-        plugin = tmuxPlugins.resurrect;
+        plugin = resurrect;
         extraConfig = ''
           set -g @resurrect-strategy-nvim 'session'
           set -g @resurrect-capture-pane-contents 'on'
         '';
       }
       {
-        plugin = tmuxPlugins.continuum;
+        plugin = continuum;
         extraConfig = ''
           set -g @continuum-restore 'on'
           set -g @continuum-save-interval '10' # Save every 10 minutes
