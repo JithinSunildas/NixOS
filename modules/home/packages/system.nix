01e80a5e2e0672aa4a53fa39a90977735fbe5e17
JithinSunildas <jithinsunildas6@gmail.com>
2026-02-03 22:48:36 +0530

    home: 2026-02-03_22:48

--- modules/home/packages/system.nix
+++ modules/home/packages/system.nix
@@ -25,38 +25,4 @@
     gnome-disk-utility
     nautilus
   ];
-programs.wlogout = {
-  enable = true;
-  # layout = [
-  # ];
-  style = ''
-    * {
-        background-image: none;
-        font-family: "JetBrainsMono Nerd Font";
-    }
-
-    window {
-        background-color: rgba(30, 30, 40, 0.7); /* Muted Sumi Ink with transparency */
-        backdrop-filter: blur(10px);
-    }
-
-    button {
-        color: #c8c093; /* Fuji White / Old Vellum */
-        background-color: rgba(22, 22, 29, 0.5);
-        border-style: solid;
-        border-width: 1px;
-        border-color: #54546d; /* Dragon Gray */
-        border-radius: 8px;
-        margin: 10px;
-        transition: all 0.3s ease-in-out;
-    }
-
-    button:focus, button:active, button:hover {
-        background-color: #c4746e; /* Dragon Red Accent */
-        color: #16161d;
-        border-color: #c4746e;
-        outline-style: none;
-    }
-  '';
-};
 }
