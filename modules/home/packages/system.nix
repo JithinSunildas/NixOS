f7183075db2b9c96c64f9422e6dbe42f24fd5123
JithinSunildas <jithinsunildas6@gmail.com>
2026-02-03 20:09:06 +0530

    home: 2026-02-03_20:09

--- modules/home/packages/system.nix
+++ modules/home/packages/system.nix
@@ -9,16 +9,17 @@
     xxd
     xhost
     ninja
+    niriswitcher
     pkg-config
     imagemagick
-    ffmpegthumbnailer 
-    unar             
-    jq               
-    poppler-utils    
-    
+    ffmpegthumbnailer
+    unar
+    jq
+    poppler-utils
+
     # File management
     yazi
-    
+
     # System monitoring/management
     gparted
     gnome-disk-utility
