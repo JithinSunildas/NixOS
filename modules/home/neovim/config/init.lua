351beece65ed307b803536a4955e29e9e107d8b1
JithinSunildas <jithinsunildas6@gmail.com>
2026-02-03 15:49:04 +0530

    home: 2026-02-03_15:49

--- modules/home/neovim/config/init.lua
+++ modules/home/neovim/config/init.lua
@@ -6,19 +6,19 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 local lazy_installed_by_nix = vim.fn.isdirectory(lazypath) == 0
 
 if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono Nerd Font:h12"
-  vim.g.neovide_scale_factor = 0.9
-  vim.g.neovide_frame = "none"
-  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+	vim.o.guifont = "JetBrainsMono Nerd Font:h12"
+	vim.g.neovide_scale_factor = 0.9
+	vim.g.neovide_frame = "none"
+	vim.g.neovide_cursor_vfx_mode = "pixiedust"
 end
 
 if lazy_installed_by_nix then
-  for _, path in ipairs(vim.api.nvim_list_runtime_paths()) do
-    if path:match("lazy%-nvim") then
-      lazypath = path
-      break
-    end
-  end
+	for _, path in ipairs(vim.api.nvim_list_runtime_paths()) do
+		if path:match("lazy%-nvim") then
+			lazypath = path
+			break
+		end
+	end
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -28,25 +28,25 @@ require("options")
 require("keymaps")
 
 require("lazy").setup("plugins.init", {
-  defaults = {
-    lazy = true,
-  },
-  install = {
-    missing = true,
-  },
-  checker = {
-    enabled = false,
-  },
-  performance = {
-    rtp = {
-      reset = false,
-    },
-  },
+	defaults = {
+		lazy = true,
+	},
+	install = {
+		missing = true,
+	},
+	checker = {
+		enabled = false,
+	},
+	performance = {
+		rtp = {
+			reset = false,
+		},
+	},
 })
 
 -- Load LSP and plugin configurations AFTER lazy setup
 require("lsp")
 require("plugins.setup")
 require("plugins.extra")
-vim.cmd("colorscheme kanso")
+vim.cmd("colorscheme kanagawa-dragon")
 vim.api.nvim_set_hl(0, "CursorLineNr", { bold = true })
--- modules/home/neovim/config/lua/plugins/init.lua
+++ modules/home/neovim/config/lua/plugins/init.lua
@@ -1,62 +1,63 @@
 -- ~/nix-config/modules/home/neovim/config/lua/plugins/init.lua
 
 return {
-  -- LSP & Completion
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-nvim-lsp",
-      "saadparwaiz1/cmp_luasnip",
-      "L3MON4D3/LuaSnip",
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-  },
+	-- LSP & Completion
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lsp",
+			"saadparwaiz1/cmp_luasnip",
+			"L3MON4D3/LuaSnip",
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+	},
 
-  -- Telescope
-  "nvim-telescope/telescope.nvim",
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  "nvim-lua/plenary.nvim",
+	-- Telescope
+	"nvim-telescope/telescope.nvim",
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	"nvim-lua/plenary.nvim",
 
-  -- Find and Replace
-  'nvim-pack/nvim-spectre',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+	-- Find and Replace
+	"nvim-pack/nvim-spectre",
+	dependencies = { "nvim-lua/plenary.nvim" },
 
-  -- File Explorer
-  "nvim-tree/nvim-tree.lua",
-  "nvim-tree/nvim-web-devicons",
+	-- File Explorer
+	"nvim-tree/nvim-tree.lua",
+	"nvim-tree/nvim-web-devicons",
 
-  -- UI
-  "nvim-lualine/lualine.nvim",
-  "goolord/alpha-nvim",
-  "lukas-reineke/indent-blankline.nvim",
-  "NvChad/nvim-colorizer.lua",
-  "karb94/neoscroll.nvim",
-  {
-    "webhooked/kanso.nvim",
-    lazy = false,
-    priority = 1000
-  },
+	-- UI
+	"nvim-lualine/lualine.nvim",
+	"goolord/alpha-nvim",
+	"lukas-reineke/indent-blankline.nvim",
+	"NvChad/nvim-colorizer.lua",
+	"karb94/neoscroll.nvim",
+	{
+		-- "webhooked/kanso.nvim
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+	},
 
-  -- Git
-  "lewis6991/gitsigns.nvim",
+	-- Git
+	"lewis6991/gitsigns.nvim",
 
-  -- Utilities
-  "windwp/nvim-autopairs",
-  "numToStr/Comment.nvim",
-  "folke/which-key.nvim",
+	-- Utilities
+	"windwp/nvim-autopairs",
+	"numToStr/Comment.nvim",
+	"folke/which-key.nvim",
 
-  -- Extra plugins
-  "nvim-flutter/flutter-tools.nvim",
-  "ahmedkhalf/project.nvim",
-  "folke/flash.nvim",
-  "j-hui/fidget.nvim",
-  "echasnovski/mini.surround",
-  "abecodes/tabout.nvim",
-  "nvim-treesitter/nvim-treesitter",
-  "nvim-treesitter/nvim-treesitter-textobjects",
+	-- Extra plugins
+	"nvim-flutter/flutter-tools.nvim",
+	"ahmedkhalf/project.nvim",
+	"folke/flash.nvim",
+	"j-hui/fidget.nvim",
+	"echasnovski/mini.surround",
+	"abecodes/tabout.nvim",
+	"nvim-treesitter/nvim-treesitter",
+	"nvim-treesitter/nvim-treesitter-textobjects",
 }
