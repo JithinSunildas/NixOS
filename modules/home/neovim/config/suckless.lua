007e2bb0b215096179048741a8917be19a8402ba
JithinSunildas <jithinsunildas6@gmail.com>
2026-02-05 23:26:44 +0530

    home: 2026-02-05_23:26

--- modules/home/neovim/config/suckless.lua
+++ modules/home/neovim/config/suckless.lua
@@ -19,7 +19,7 @@ vim.opt.rtp:prepend(lazypath)
 require("options")
 
 -- theme & transparency
-vim.cmd("colorscheme kanagawa-dragon")
+vim.cmd.colorscheme("unokai")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
