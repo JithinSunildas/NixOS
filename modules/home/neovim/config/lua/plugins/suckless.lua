cfc753e478a53993e2d2a801b66232cc1f40e4ab
JithinSunildas <jithinsunildas6@gmail.com>
2026-02-06 16:25:56 +0530

    home: 2026-02-06_16:25

--- modules/home/neovim/config/lua/plugins/suckless.lua
+++ modules/home/neovim/config/lua/plugins/suckless.lua
@@ -1,43 +1,42 @@
 -- ~/nix-config/modules/home/neovim/config/lua/plugins/init.lua
 
 return {
-  -- LSP & Completion
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-nvim-lsp",
-      "saadparwaiz1/cmp_luasnip",
-      "L3MON4D3/LuaSnip",
+    -- LSP & Completion
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-nvim-lsp",
+            "saadparwaiz1/cmp_luasnip",
+            "L3MON4D3/LuaSnip",
+        },
     },
-  },
-  {
-    "neovim/nvim-lspconfig",
-  },
+    {
+        "neovim/nvim-lspconfig",
+    },
 
-  -- Telescope
-  "nvim-telescope/telescope.nvim",
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  "nvim-lua/plenary.nvim",
+    -- Telescope
+    "nvim-telescope/telescope.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-lua/plenary.nvim",
 
-  -- UI
-  {
-    -- "webhooked/kanso.nvim
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-  },
+    -- UI
+    {
+        -- "webhooked/kanso.nvim
+        "rebelot/kanagawa.nvim",
+        lazy = false,
+        priority = 1000,
+    },
 
-  -- Git
-  "lewis6991/gitsigns.nvim",
+    -- Git
+    "lewis6991/gitsigns.nvim",
 
-  -- Utilities
-  "windwp/nvim-autopairs",
+    -- Utilities
+    "windwp/nvim-autopairs",
 
-  -- Extra plugins
-  "folke/flash.nvim",
-  "nvim-treesitter/nvim-treesitter",
-  "nvim-treesitter/nvim-treesitter-textobjects",
+    -- Extra plugins
+    "folke/flash.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter-textobjects",
 }
