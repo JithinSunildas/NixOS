5c18dc9516487ef34dde8848f9c6b2b44b91d528
JithinSunildas <jithinsunildas6@gmail.com>
2026-02-07 14:41:49 +0530

    home: 2026-02-07_14:41

--- modules/home/neovim/config/lua/file_spec_config.lua
+++ modules/home/neovim/config/lua/file_spec_config.lua
@@ -1,18 +1,21 @@
 -- Set filetype-specific settings
+
+local augroup = vim.api.nvim_create_augroup("UserConfig", {})
+
 vim.api.nvim_create_autocmd("FileType", {
-  group = augroup,
-  pattern = { "lua", "python" },
-  callback = function()
-    vim.opt_local.tabstop = 4
-    vim.opt_local.shiftwidth = 4
-  end,
+    group = augroup,
+    pattern = { "lua", "python", "haskell" },
+    callback = function()
+        vim.opt_local.tabstop = 4
+        vim.opt_local.shiftwidth = 4
+    end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  group = augroup,
-  pattern = { "javascript", "typescript", "json", "html", "css" },
-  callback = function()
-    vim.opt_local.tabstop = 2
-    vim.opt_local.shiftwidth = 2
-  end,
+    group = augroup,
+    pattern = { "javascript", "typescript", "json", "html", "css" },
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+    end,
 })
