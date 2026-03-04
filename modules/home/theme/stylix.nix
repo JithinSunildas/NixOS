59849c563b3029fa42756698eb4d223d605509ad
JithinSunildas <jithinsunildas6@gmail.com>
2026-03-04 19:55:23 +0530

    home: 2026-03-04_19:55

--- modules/home/theme/stylix.nix
+++ modules/home/theme/stylix.nix
@@ -3,7 +3,7 @@
 {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
     image = ./wallpapers/secluded-grove-pixel.png;
     polarity = "dark";
 
