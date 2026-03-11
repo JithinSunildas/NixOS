e9735364023eedb03f438db1f11ddd50a0e2e740
JithinSunildas <jithinsunildas6@gmail.com>
2026-03-11 20:29:08 +0530

    home: 2026-03-11_20:29

--- modules/home/waybar/waybar.nix
+++ modules/home/waybar/waybar.nix
@@ -8,4 +8,5 @@
 {
   xdg.configFile."waybar/config".source = 
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/modules/home/waybar/waybar.conf";
+  home.file.".config/waybar/style.css".source = ./waybar.css;
 }
