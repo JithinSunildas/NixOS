0572e30f81ded4278d7ee40521ba1df15d5cd353
JithinSunildas <jithinsunildas6@gmail.com>
2026-03-11 20:16:47 +0530

    home: 2026-03-11_20:16

--- modules/home/niri/niri.nix
+++ modules/home/niri/niri.nix
@@ -11,14 +11,14 @@
         font = "Libre Baskerville:size=12";
         terminal = "foot"; 
       };
+      colors = {
+          background = "0f0f0fff";     # Deep black-gray
+          text = "d1d1d1ff";           # Soft white/light gray
+          match = "ffffffff";          # Pure white for matched characters (pop)
+          selection = "2a2a2aff";      # Subtle dark gray for selection
+          selection-text = "ffffffff"; # White text when selected
+          border = "333333ff";         # Dark gray border
+        };
     };
-    colors = {
-        background = "0f0f0fff";     # Deep black-gray
-        text = "d1d1d1ff";           # Soft white/light gray
-        match = "ffffffff";          # Pure white for matched characters (pop)
-        selection = "2a2a2aff";      # Subtle dark gray for selection
-        selection-text = "ffffffff"; # White text when selected
-        border = "333333ff";         # Dark gray border
-      };
   };
 }
