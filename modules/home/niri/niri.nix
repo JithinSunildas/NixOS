4c4c6c37e9265135ab5c06b899d6d76fa3fab834
JithinSunildas <jithinsunildas6@gmail.com>
2026-03-11 20:20:24 +0530

    home: 2026-03-11_20:20

--- modules/home/niri/niri.nix
+++ modules/home/niri/niri.nix
@@ -8,17 +8,17 @@
     enable = true;
     settings = {
       main = {
-        font = "Libre Baskerville:size=10";
+        font = "Libre Baskerville:size=11";
         terminal = "foot"; 
       };
       colors = {
-          background = "0f0f0fff";     
-          text = "d1d1d1ff";           
-          match = "ffffffff";          
-          selection = "2a2a2aff";      
-          selection-text = "ffffffff"; 
-          border = "333333ff";         
-        };
+        background = "0f0f0fcc";     
+        text = "d1d1d1ff";           
+        match = "ffffffff";          
+        selection = "2a2a2acc";      
+        selection-text = "ffffffff"; 
+        border = "333333ff";         
+      }; 
     };
   };
 }
