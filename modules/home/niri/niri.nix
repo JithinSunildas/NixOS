c2d519cbebe3d2287836e41b705da736da55480f
JithinSunildas <jithinsunildas6@gmail.com>
2026-03-11 19:59:11 +0530

    home: 2026-03-11_19:59

--- modules/home/niri/niri.nix
+++ modules/home/niri/niri.nix
@@ -2,5 +2,5 @@
 
 {
   xdg.configFile."niri/config.kdl".source = 
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/modules/home/niri/config.kdl";
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/modules/home/niri/";
 }
