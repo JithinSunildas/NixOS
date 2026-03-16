02fd1d8361bcb51b00a8d0b05263f594edcae38f
JithinSunildas <jithinsunildas6@gmail.com>
2026-03-16 21:50:39 +0530

    home: 2026-03-16_21:50

--- modules/home/packages/development.nix
+++ modules/home/packages/development.nix
@@ -1,51 +1,53 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    # Editors
+# Editors
     helix
-    vscode
-    emacs
+      vscode
+      emacs
 
-    # Build tools
-    cmake
-    pkgsCross.musl64.stdenv.cc
+# Build tools
+      cmake
+      pkgsCross.musl64.stdenv.cc
 
-    # Design and Frontend
-    typst
-    tinymist
-    pandoc
+# Design and Frontend
+      typst
+      tinymist
+      pandoc
 
-    # Hardware design
-    iverilog
+# Hardware design
+      iverilog
 
-    # Mobile
-    flutter
-    android-tools
+# Mobile
+      flutter
+      android-tools
 
-    # Backend
-    # Java/Spring Boot
-    jdk21
-    maven
-    gradle
-    spring-boot-cli
-    mariadb
+# Backend
+# Java/Spring Boot
+      jdk21
+      maven
+      gradle
+      spring-boot-cli
+      mariadb
 
-    # PHP/Laravel
-    php83
-    php83Extensions.pdo
-    php83Extensions.mbstring
-    php83Extensions.xml
-    php83Extensions.curl
-    php83Extensions.zip
-    php83Extensions.gd
+# PHP/Laravel
+      php83
+      php83Extensions.pdo
+      php83Extensions.mbstring
+      php83Extensions.xml
+      php83Extensions.curl
+      php83Extensions.zip
+      php83Extensions.gd
 
-    # Databases
-    mariadb
-    postgresql
-  ];
+# Databases
+      mariadb
+      postgresql
+      ];
 
-  CC_x86_64_unknown_linux_musl = "${pkgs.pkgsCross.musl64.stdenv.cc}/bin/x86_64-unknown-linux-musl-gcc";
-  CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${pkgs.pkgsCross.musl64.stdenv.cc}/bin/x86_64-unknown-linux-musl-gcc";
+  home.sessionVariables = {
+    CC_x86_64_unknown_linux_musl = "${pkgs.pkgsCross.musl64.stdenv.cc}/bin/x86_64-unknown-linux-musl-gcc";
+    CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${pkgs.pkgsCross.musl64.stdenv.cc}/bin/x86_64-unknown-linux-musl-gcc";
+  };
 
   nixpkgs.config = {
     android_sdk.accept_license = true;
