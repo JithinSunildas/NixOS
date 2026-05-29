15b3da7b60f5ef021aa58e31d1e10ad2970d9344
JithinSunildas <jithinsunildas6@gmail.com>
2026-05-29 17:46:03 +0530

    home: 2026-05-29_17:46

--- modules/home/mango/mango.nix
+++ modules/home/mango/mango.nix
@@ -1,6 +1,6 @@
-{ }:
-
-{
-  # xdg.configFile."mango/config.conf".source =
-  #   config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/modules/home/mango/config.conf";
-}
+# { }:
+#
+# {
+#   # xdg.configFile."mango/config.conf".source =
+#   #   config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/modules/home/mango/config.conf";
+# }
