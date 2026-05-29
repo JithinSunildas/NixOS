8961e7ad030b9937589fc349e05eab091ebf2e9b
JithinSunildas <jithinsunildas6@gmail.com>
2026-05-29 11:46:56 +0530

    home: 2026-05-29_11:46

--- modules/home/xcompose/xcompose.nix
+++ modules/home/xcompose/xcompose.nix
@@ -9,5 +9,10 @@
     GTK_IM_MODULE = "xim";
     QT_IM_MODULE = "xim";
     XMODIFIERS = "@im=none";
-  };
+
+    NIXOS_OZONE_WL = "1";
+    XDG_SESSION_TYPE = "wayland";
+    
+    GDK_BACKEND = "wayland";
+    };
 }
