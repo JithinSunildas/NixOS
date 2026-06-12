119b0f813aeb1e583c84b1047d0419c135886320
JithinSunildas <jithinsunildas6@gmail.com>
2026-06-12 13:40:35 +0530

    home: 2026-06-12_13:40

--- modules/home/theme/stylix.nix
+++ modules/home/theme/stylix.nix
@@ -27,7 +27,7 @@
       };
       monospace = {
         package = pkgs.nerd-fonts.iosevka-term-slab;
-        name = "IosevkaTermSlab Nerd Font Mono";
+        # name = "IosevkaTermSlab Nerd Font Mono";
       };
 
       sizes = {
