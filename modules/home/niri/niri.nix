be85fed9551f5509ac3674007594976139c54180
JithinSunildas <jithinsunildas6@gmail.com>
2026-06-13 19:03:24 +0530

    home: 2026-06-13_19:03

--- modules/home/niri/niri.nix
+++ modules/home/niri/niri.nix
@@ -5,7 +5,7 @@
     niri
   ];
 
-  programs.niri.enable = true;
+  # programs.niri.enable = true;
 
   xdg.configFile."niri/config.kdl".source = 
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/modules/home/niri/config.kdl";
